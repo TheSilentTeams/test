e9740c4c83a98ccfc6256fa3a7511bd22582a07e
TheSilentTeams <shivcollegelife@gmail.com>
2025-06-30 21:54:45 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,6 +8,7 @@ from datetime import datetime, timedelta
 from pymongo import MongoClient
 from pyrogram import Client, filters, utils
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from urllib.parse import quote_plus
 from bs4 import BeautifulSoup
 
 # === Config ===
@@ -305,8 +306,13 @@ async def handle_any_message(client, message):
             files = await terabox(url)
             for dlink, name, size, thumb in files:
                 text = f"\n✅ **File:** {name}\n📦 **Size:** {size}\n"
-                buttons = InlineKeyboardMarkup(
-                    [[InlineKeyboardButton("⬇️ Download ⬇️", url=dlink)]])
+                proxy_link = f"https://thesilentteams.shivcollegelife.workers.dev/?url={quote_plus(dlink)}"
+                buttons = InlineKeyboardMarkup([
+                    [
+                        InlineKeyboardButton("⬇️ Direct Link", url=dlink),
+                        InlineKeyboardButton("⚡ Proxy Download", url=proxy_link)
+                    ]
+                ])
                 await client.send_photo(
                     message.chat.id,
                     thumb
